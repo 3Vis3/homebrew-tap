5ff2c611b4fbba4af36727f97a02e00a758aa719
3Vis <francescotrevisan3@gmail.com>
2026-05-07 16:38:36 +0800

    Update nordvpn-macos-cli to v0.3.0

--- Formula/nordvpn-macos-cli.rb
+++ Formula/nordvpn-macos-cli.rb
@@ -1,11 +1,12 @@
 class NordvpnMacosCli < Formula
   desc "Control NordVPN manual VPN profiles on macOS"
   homepage "https://github.com/3Vis3/nordvpn-macos-cli"
-  url "https://github.com/3Vis3/nordvpn-macos-cli/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "deaa17a3d1801bd7655b54aad77dd26484d9b43604aeddd57bf085a88e241aaf"
+  url "https://github.com/3Vis3/nordvpn-macos-cli/archive/refs/tags/v0.3.0.tar.gz"
+  sha256 "0afc5a705e52ecc3ef840629817656a570f4806492963655cd5e1b6ff8340303"
   license "MIT"
 
   depends_on xcode: ["13.0", :build]
+  depends_on "openvpn"
 
   def install
     system "swift", "build", "-c", "release", "--disable-sandbox"
